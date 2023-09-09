96dfdff8d823e9bb58981f2468c7beadbb155231
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2023-09-09 06:56:28 -0700

    Update doc-aio-canvas.js

--- src/documents/aio-canvas/doc-aio-canvas.js
+++ src/documents/aio-canvas/doc-aio-canvas.js
@@ -4,7 +4,7 @@ import AIOInput from "../../npm/aio-input/aio-input";
 import Canvas from './../../npm/aio-canvas/aio-canvas';
 import {Icon} from '@mdi/react';
 import AIODoc from './../../npm/aio-documentation/aio-documentation';
-import { mdiChevronDown, mdiChevronLeft, mdiChevronRight, mdiChevronUp, mdiCircle, mdiClose, mdiCodeJson, mdiDelete, mdiEye, mdiEyeOff, mdiPlusThick } from "@mdi/js";
+import { mdiChevronDown, mdiChevronLeft, mdiChevronRight, mdiChevronUp, mdiCircle, mdiClose, mdiCodeJson, mdiContentCopy, mdiDelete, mdiEye, mdiEyeOff, mdiPlusThick } from "@mdi/js";
 import './index.css';
 const CTX = createContext()
 function Reducer(state,action){
@@ -12,9 +12,8 @@ function Reducer(state,action){
 }
 export default function DOC_AIO_Canvas(){
     let [state,dispatch] = useReducer(Reducer,{
-        items:[
-        ],
-        types:['Arc','Rectangle'],
+        items:[],
+        types:['Group','Arc','Rectangle'],
         activeItemId:false
     })
     function header_layout(){
@@ -28,34 +27,80 @@ export default function DOC_AIO_Canvas(){
             html:<Body/>
         }
     }
+    
     function footer_layout(){
         return {
             html:<Footer/>
         }
     }
+    function getNewId(){return 'aa' + Math.round(Math.random() * 10000000);}
+    function getItemById(ids){
+        let {items} = state;
+        let result;
+        for(let i = 0; i < ids.length; i++){
+            result = items.find((o)=>o.id[i] === ids[i]);
+            items = result.items;
+        }
+        return result
+    }
+    function getParentById(id){
+        if(id.length === 1){return false}
+        return getItemById(id.slice(0,id.length - 1));
+    }
+    function changeItems(newItems){dispatch({key:'items',value:newItems})}
+    function changeItem(item,obj){
+        let {items} = state;
+        for(let prop in obj){
+            item[prop] = obj[prop]
+        }
+        dispatch({key:'items',value:items})
+    }
+    function addNewItem(type,parent){
+        let id = getNewId();
+        let newItem = {type,id:parent?[...parent.id,id]:[id],name:type + Math.round(Math.random() * 10000000),x:0,y:0,lineWidth:1,show:true,rotate:0}
+        if(type === 'Arc'){
+            newItem = {...newItem,r:100}
+        }
+        else if(type === 'Rectangle'){
+            newItem = {...newItem,width:100,height:100}
+        }
+        else if(type === 'Group'){
+            newItem = {...newItem,items:[],open:true}
+        }
+        addItem(newItem,parent)
+    }
+    function addItem(newItem,parent){
+        let {items} = state;
+        if(parent){changeItem(parent,{items:[...parent.items,newItem]})}
+        else {changeItems(items.concat(newItem))}
+    }
+    function removeItem(item){
+        let {items} = state;
+        let parent = getParentById(item.id);
+        if(parent){changeItem(parent,{items:parent.items.filter((o)=>o.id.toString() !== item.id.toString())})}
+        else {changeItems(items.filter((o)=>o.id.toString() !== item.id.toString()))}
+        setActiveItemId(false)
+    }
+    function cloneItem(item){
+        let parent = getParentById(item.id);
+        let newId = parent?[...parent.id,getNewId()]:[getNewId()]
+        let newItem = {...item,id:newId,name:item.name + ' (clone)'};
+        addItem(newItem,parent)
+    }
+    function setActiveItemId(id){
+        debugger
+        if(!state.activeItemId){dispatch({key:'activeItemId',id})}
+        else if(state.activeItemId.toString() !== id.toString()){
+            dispatch({key:'activeItemId',id})
+        }
+    }
     function getContext(){
         return {
             ...state,
-            changeItem:(item,obj)=>{
-                let {items} = state;
-                for(let prop in obj){
-                    item[prop] = obj[prop]
-                }
-                dispatch({key:'items',value:items})
-            },
-            addItem:(type)=>{
-                let {items} = state;
-                let id = 'aa' + Math.round(Math.random() * 10000000);
-                let item = {type,id,name:type + Math.round(Math.random() * 10000000),x:0,y:0,lineWidth:1,show:true}
-                if(type === 'Arc'){
-                    item = {...item,r:100}
-                }
-                else if(type === 'Rectangle'){
-                    item = {...item,width:100,height:100}
-                }
-                dispatch({key:'items',value:items.concat(item)})
-            },
-            setActiveItemId:(id)=>dispatch({key:'activeItemId',value:state.activeItemId === id?false:id})
+            changeItem,
+            addNewItem,
+            addItem,removeItem,cloneItem,getItemById,
+            setActiveItemId
         }
     }
     return (
@@ -127,11 +172,12 @@ function Footer(){
     )
 }
 function Items(){
+    let {items} = useContext(CTX);
     function header_layout(){
         return {html:<Items_Header/>}
     }
     function body_layout(){
-        return {flex:1,html:<Items_Body/>}
+        return {flex:1,html:<Items_Body items={items} level={0}/>}
     }
     function footer_layout(){
         return {html:'items footer'}
@@ -149,26 +195,13 @@ function Items(){
     )
 }
 function Items_Header(){
-    let {types,addItem} = useContext(CTX)
     return (
         <RVD
             layout={{
                 className:'aioc-items-header',
                 row:[
                     {
-                        size:36,html:(
-                            <AIOInput
-                                type='select' caret={false}
-                                text={<Icon path={mdiPlusThick} size={.7}/>}
-                                options={types}
-                                optionText='option'
-                                optionValue='option'
-                                popover={{
-                                    attrs:{style:{color:'#333'}}
-                                }}
-                                onChange={(type)=>addItem(type)}
-                            />
-                        ),align:'vh'
+                        size:36,html:<AddItem/>,align:'vh'
                     },
                     {flex:1},
                     {
@@ -179,26 +212,48 @@ function Items_Header(){
         />
     )
 }
-function Items_Body(){
-    let {items} = useContext(CTX);
+function AddItem(props = {}){
+    let {parent} = props;
+    let {types,addNewItem} = useContext(CTX)
+    return (
+        <AIOInput
+            type='select' caret={false}
+            text={<Icon path={mdiPlusThick} size={.7}/>}
+            options={types}
+            optionText='option'
+            optionValue='option'
+            popover={{
+                attrs:{style:{color:'#333'}}
+            }}
+            onChange={(type)=>addNewItem(type,parent)}
+        />
+    )
+}
+function Items_Body({items,level}){
     return (
         <RVD
             layout={{
                 column:items.map((item,i)=>{
                     return {
-                        html:<Item key={item.id} item={item}/>
+                        column:[
+                            {html:<Item key={item.id} item={item} level={level}/>},
+                            {show:item.type === 'Group',html:()=><Items_Body items={item.items} level={level + 1}/>}
+                        ]
                     }
                 })
             }}
         />
     )
 }
-function Item({item}){
+function Item({item,level}){
     let {changeItem,activeItemId,setActiveItemId} = useContext(CTX)
     return (
         <AIOInput
             className={'aioc-item' + (activeItemId === item.id?' active':'')}
-            before={<Icon path={mdiCircle} size={.4}/>}
+            before={<Icon path={item.type === 'Group'?(item.open?mdiChevronDown:mdiChevronRight):mdiCircle} size={.8} onClick={item.type !== 'Group'?undefined:(e)=>{
+                e.stopPropagation();
+                changeItem(item,{open:!item.open})
+            }}/>}
             after={(
                 <RVD
                     layout={{
@@ -212,6 +267,7 @@ function Item({item}){
                 />
             )}
             type='text'
+            style={{paddingLeft:level * 12}}
             value={item.name}
             onChange={(name)=>changeItem(item,{name})}
             onClick={()=>setActiveItemId(item.id)}
@@ -235,7 +291,7 @@ function Preview(){
     )
 }
 function Setting(){
-    let {activeItemId,items,changeItem,setActiveItemId} = useContext(CTX);
+    let {activeItemId,items,changeItem,setActiveItemId,removeItem,cloneItem,getItemById} = useContext(CTX);
     let [codeView,setCodeView] = useState(false);
     function header_layout(activeItem){
         return {
@@ -251,9 +307,9 @@ function Setting(){
         return {
             className:'aioc-setting-header',align:'v',
             row:[
-                {html:`${activeItem.name} ( ${activeItem.type} )`,flex:1},
-                {html:<Icon path={mdiCodeJson} size={.7}/>,align:'vh',onClick:()=>setCodeView(!codeView),size:24,style:{height:24,background:codeView?'orange':undefined}},
-                {html:<Icon path={mdiClose} size={.7}/>,align:'vh',onClick:()=>setActiveItemId(false),size:24}
+                {show:activeItem.type === 'Group',html:<AddItem parent={activeItem}/>,align:'vh',size:24},
+                {html:<Icon path={mdiContentCopy} size={.7}/>,align:'vh',onClick:()=>cloneItem(activeItem),size:24},
+                {html:<Icon path={mdiDelete} size={.7}/>,align:'vh',onClick:()=>removeItem(activeItem),size:24}
             ]
         }
     }
@@ -299,6 +355,16 @@ function Setting(){
                                 ]
                             },
                             {
+                                show:activeItem.type === 'Rectangle',
+                                row:[
+                                    {input:{type:'number'},field:'value.corner[0]',label:'corner'},
+                                    {input:{type:'number'},field:'value.corner[1]',label:'corner'},
+                                    {input:{type:'number'},field:'value.corner[2]',label:'corner'},
+                                    {input:{type:'number'},field:'value.corner[3]',label:'corner'}, 
+                                    {html:<Icon path={mdiDelete} size={.7}/>,align:'vh',style:{paddingTop:16},onClick:()=>changeItem(activeItem,{corner:undefined})},
+                                ]
+                            },
+                            {
                                 row:[
                                     {input:{type:'number'},field:'value.slice[0]',label:'slice from'},
                                     {input:{type:'number'},field:'value.slice[1]',label:'slice to'},
@@ -322,8 +388,8 @@ function Setting(){
             )
         }
     }
-    if(activeItemId === false){return null}
-    let activeItem = items.find((o)=>o.id === activeItemId)
+    if(!activeItemId){return null}
+    let activeItem = getItemById(activeItemId)
     return (
         <RVD
             layout={{
@@ -332,7 +398,8 @@ function Setting(){
                     header_layout(activeItem),
                     form_layout(activeItem),
                     code_layout(activeItem),
-
+                    footer_layout(activeItem),
+                    
                 ]
             }}
         />
