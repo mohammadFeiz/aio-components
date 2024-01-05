db2f22906810ddb6dbba519df647aaf7a482d129
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2024-01-06 01:41:43 +0330

    .

--- src/documents/msfloading1/msfloading1.js
+++ src/documents/msfloading1/msfloading1.js
@@ -1,30 +1,31 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import RVD from './../../npm/react-virtual-dom/react-virtual-dom';
 import AIOInput from './../../npm/aio-input/aio-input';
 import AIOPopup from './../../npm/aio-popup/aio-popup';
-import {Icon} from '@mdi/react';
-import { mdiContentCopy, mdiEye,mdiEyeOff } from '@mdi/js';
+import { FileToBase64 } from '../../npm/aio-utils';
+import { Icon } from '@mdi/react';
+import { mdiContentCopy, mdiEye, mdiEyeOff } from '@mdi/js';
 import './index.css';
 import $ from 'jquery';
-import file from './image.png'
-export default function DOC_MSFLoading1({goToHome}) {
+export default function DOC_MSFLoading1({ goToHome }) {
     let [popup] = useState(new AIOPopup())
-    let [model,setModel] = useState({
-        count:9,
-        rtl:true,
-        duration:1,
-        loop:false,
-        offset:4,
-        size:36,
-        loopDelay:0.7,
-        className:'bmloading',
-        sizes:[20, 16, 16, 16, 16, 16, 16, 16, 16],
-        border:true,
-        containerSize:300,
-        activeSize:false
+    let [file,setFile] = useState();
+    let [model, setModel] = useState({
+        count: 9,
+        rtl: true,
+        duration: 1,
+        loop: false,
+        offset: 4,
+        size: 36,
+        loopDelay: 0.7,
+        className: '',
+        sizes: [20, 16, 16, 16, 16, 16, 16, 16, 16],
+        border: true,
+        containerSize: 300,
+        activeSize: false
     })
     function getCss() {
-        let {count, duration, loop, offset, size, className, loopDelay, sizes, border} = model;
+        let { count, duration, loop, offset, size, className, loopDelay, sizes } = model;
         let str = '';
         let start = 0;
         let delay = loopDelay / (duration + loopDelay) * 100
@@ -40,7 +41,6 @@ export default function DOC_MSFLoading1({goToHome}) {
     object-fit:cover;
     object-position:top 0px right ${-unitSizes}px;
     ${loop ? 'animation-iteration-count:infinite;' : ''}
-    ${border ? `border:1px solid ${model.activeSize === i?'red':'#00000030'};` : ''}
 }
 @keyframes ${className}${i + 1} {
     0% {
@@ -69,159 +69,159 @@ export default function DOC_MSFLoading1({goToHome}) {
         return str
     }
     function getJs() {
-        let {count, className, sizes,containerSize} = model;
+        let { count, className, sizes, containerSize } = model;
         let str = `
-import lodingsrc from './image.png';
+import imagesrc from './image.png';
+import './loading.css';
 export default function Loading(){
     return (
         <div className="${className}" style={{display:"flex",width:${containerSize}}}>`
-for(let i = 0; i < count; i++){
-    str+=(`
-            <img src={loadingsrc} className="bmloadingitem" style={{width: ${sizes[i]}}}/>`)
-}
+        for (let i = 0; i < count; i++) {
+            str += (`
+            <img src={imagesrc} className="${className}item" style={{width: ${sizes[i]}}}/>`)
+        }
         str += `
         </div>`
         str += `
-    )` 
+    )`
         str += `
-}`      
+}`
         return str
     }
-    function code_layout(){
-        let css = getCss()
-        let js = getJs();
+    function codes_layout() {
         return {
-            size:240,
-            style:{direction:'ltr',border:'1px solid #ddd'},
-            row:[
-                {flex:1,html:(<pre className='of-auto w-100'>{css}</pre>)},
+            column: [
+                {className:'p-3 p-b-0',html: <AIOInput type='button' text={'Exit To Home'} onClick={()=>goToHome()} className='ai-style-4 fs-14 m-0'/>},
+                {className:'p-3 p-b-0',html: <AIOInput type='file' text={'Select Image'} onChange={(file)=>FileToBase64(file,(file)=>setFile(file))} className='ai-style-4 fs-14 m-0'/>},
+                { className:'p-3',flex: 1, row: [code_layout('JS', getJs()), { size: 3 }, code_layout('CSS', getCss())] },
+                
+            ]
+        }
+    }
+    function isThereError(){
+        if(!model.className){return 'please set className'}
+    }
+    function code_layout(type, code) {
+        let error = isThereError();
+        return {
+            size: 72, className: 'fs-18 br-6 m-0',
+            style: { background: !!error?'#ddd':'dodgerblue', color: '#fff', padding: 6 },
+            column: [
+                { html: 'COPY', align: 'h' }, { html: type, align: 'h' },
                 {
-                    style:{background:'dodgerblue',color:'#fff',padding:6},
-                    align:'vh',html:<Icon path={mdiContentCopy} size={2}/>,
-                    onClick:()=>{
-                        window.navigator.clipboard.writeText(css);
-                        popup.addSnakebar({
-                            type:'success',time:2,rtl:true,style:{opacity:0.5},
-                            text:'کد های سی اس اس در کلیپ بورد ذخیره شد'
-                        })
-                    }
-                },
-                {flex:1,html:(<pre className='of-auto w-100 fs-10'>{js}</pre>)},
-                {
-                    style:{background:'dodgerblue',color:'#fff',padding:6},
-                    align:'vh',html:<Icon path={mdiContentCopy} size={2}/>,
-                    onClick:()=>{
-                        window.navigator.clipboard.writeText(js);
-                        popup.addSnakebar({
-                            type:'success',time:2,rtl:true,style:{opacity:0.5},
-                            text:'کد های جاوا اسکریپت در کلیپ بورد ذخیره شد'
-                        })
+                    flex: 1, align: 'vh', html: <Icon path={mdiContentCopy} size={1.8} />,
+                    onClick: () => {
+                        if(error){alert(error); return}
+                        window.navigator.clipboard.writeText(code);
+                        popup.addSnakebar({ type: 'success', time: 2, rtl: true, style: { opacity: 0.5 }, text: `${type} codes is copied in clipboard` })
                     }
                 }
             ]
-        }       
+        }
     }
-    function changeSizes(n){
-        let newSizes = model.sizes.map((o)=>o + n)
-        setModel({...model,sizes:newSizes})
-    }
-    function form_layout(){
+    function form_layout() {
         return {
-            style:{direction:'ltr'},size:240,
-            html:(
+            flex: 1, style: { direction: 'ltr' },
+            html: (
                 <AIOInput
                     type='form'
-                    onChange={(model)=>setModel(model)}
-                    value={{...model}}
+                    onChange={(model) => setModel(model)}
+                    value={{ ...model }}
                     inputs={{
-                        props:{gap:12},
-                        column:[
+                        props: { gap: 12 },
+                        column: [
                             {
-                                row:[
-                                    {input:{type:'number'},label:'Count',field:'value.count'},
-                                    {input:{type:'number'},label:'Size',field:'value.size'},
-                                    {input:{type:'number'},label:'Container Width',field:'value.containerSize'},
-                                    {input:{type:'number'},label:'Offset',field:'value.offset'},
-                                    {input:{type:'number'},label:'Loop Delay',field:'value.loopDelay'},
-                                    
+                                row: [
+                                    { input: { type: 'number' }, label: 'Count', field: 'value.count' },
+                                    { input: { type: 'number' }, label: 'Size', field: 'value.size' },
+                                    { input: { type: 'number' }, label: 'Container Width', field: 'value.containerSize' },
+                                    { input: { type: 'number' }, label: 'Offset', field: 'value.offset' },
+                                    { input: { type: 'number' }, label: 'Loop Delay', field: 'value.loopDelay' },
+
                                 ]
                             },
                             {
-                                row:[
-                                    {input:{type:'number'},label:'Duration',field:'value.duration'},
-                                    {input:{type:'checkbox',text:'Border'},label:'Border',field:'value.border'},
-                                    {input:{type:'checkbox',text:'Loop'},label:'Loop',field:'value.loop'},
-                                    {input:{type:'checkbox',text:'RTL'},label:'RTL',field:'value.rtl'},
-                                    {input:{type:'text'},label:'ClassName',field:'value.className'},
-                                    
+                                row: [
+                                    { input: { type: 'number' }, label: 'Duration', field: 'value.duration' },
+                                    { input: { type: 'checkbox', text: 'Border' }, label: 'Border', field: 'value.border' },
+                                    { input: { type: 'checkbox', text: 'Loop' }, label: 'Loop', field: 'value.loop' },
+                                    { input: { type: 'checkbox', text: 'RTL' }, label: 'RTL', field: 'value.rtl' },
+                                    { input: { type: 'text',filter:[' ', '.','_','1','2','3','4','5','6','7','8','9','0'] }, label: 'ClassName', field: 'value.className' },
+
                                 ]
                             },
                             {
-                                row:[
-                                    {html:'sizes',align:'v'},
+                                row: [
+                                    { html: 'sizes', align: 'v' },
                                     {
-                                        row:[
+                                        flex:1,
+                                        row: [
                                             {
-                                                row:model.sizes.map((s,i)=>{
+                                                flex:1,props: { gap: 3 },className:'ofx-auto',
+                                                row: new Array(model.count).fill(0).map((s, i) => {
                                                     let active = model.activeSize === i;
                                                     return {
-                                                        column:[
+                                                        style: { border: '1px solid #ddd' },
+                                                        className: 'p-3 br-6',
+                                                        column: [
                                                             {
-                                                                style:{color:active?'dodgerblue':'#ddd'},
-                                                                html:<Icon path={active?mdiEye:mdiEyeOff} size={.8}/>,align:'vh',
-                                                                onClick:()=>setModel({...model,activeSize:i})
+                                                                style: { color: active ? 'dodgerblue' : '#ddd' },
+                                                                html: <Icon path={active ? mdiEye : mdiEyeOff} size={.8} />, align: 'vh',
+                                                                onClick: () => setModel({ ...model, activeSize: model.activeSize === i ? false : i })
                                                             },
-                                                            {input:{type:'number',style:{width:60}},field:`value.sizes[${i}]`}
+                                                            { input: { type: 'number', style: { width: 60 } }, field: `value.sizes[${i}]` }
                                                         ]
                                                     }
                                                 })
-                                            },
-                                            {
-                                                align:'vh',size:48,style:{border:'1px solid #ddd',background:'dodgerblue',color:'#fff',border:'none',borderRadius:12},
-                                                html:'-1',onClick:()=>changeSizes(-1)
-                                            },
-                                            {
-                                                align:'vh',size:48,style:{border:'1px solid #ddd',background:'dodgerblue',color:'#fff',border:'none',borderRadius:12},
-                                                html:'+1',onClick:()=>changeSizes(1)
-                                            },
-
+                                            }
                                         ]
                                     }
                                 ]
                             }
-                                    
+
                         ]
                     }}
                 />
             )
         }
     }
+    function getBorder(index) {
+        if (model.activeSize === index) { return `1px solid red` }
+        if (model.border) { return `1px solid #dddddd` }
+    }
     return (
         <>
             <RVD
-            layout={{
-                className:'fullscreen',
-                column:[
-                    form_layout(),
-                    code_layout(),
-                    {
-                        style:{direction:model.rtl?'rtl':'ltr'},
-                        flex:1,align:'vh',
-                        html:(
-                            <div className={model.className} style={{display:'flex',width:model.containerSize,border:'1px solid'}}>
-                                {new Array(model.count).fill(0).map((o, i) => {
-                                    let width = model.sizes[i]
-                                    return (
-                                        <img src={file} className={`${model.className}item`} style={{ width }} />
-                                    )
-                                })}
-                            </div>
-                        )
-                    }
-                ]
-            }}
-        />
-        {popup.render()}
+                layout={{
+                    className: 'fullscreen',
+                    column: [
+                        {
+                            style: { border: '3px solid dodgerblue' },
+                            className: 'br-12 m-12 m-b-0',
+                            row: [
+                                form_layout(),
+                                codes_layout()
+                            ]
+                        },
+                        {
+                            style: { border: '3px solid dodgerblue', background: 'dodgerblue', direction: model.rtl ? 'rtl' : 'ltr' },
+                            className: 'br-12 m-12 p-12',
+                            flex: 1, align: 'vh',
+                            html: (
+                                <div className={model.className} id='doc-msfloading1-container' style={{ width: model.containerSize }}>
+                                    {new Array(model.count).fill(0).map((o, i) => {
+                                        let width = model.sizes[i]
+                                        return (
+                                            <img src={file} className={`${model.className}item`} style={{ width, border: getBorder(i) }} />
+                                        )
+                                    })}
+                                </div>
+                            )
+                        }
+                    ]
+                }}
+            />
+            {popup.render()}
         </>
     )
 }
