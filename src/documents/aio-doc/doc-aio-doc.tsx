01284309bfc018d72e011419e7efa5d323e0103e
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2024-05-22 13:48:57 +0330

    .

--- src/documents/aio-doc/doc-aio-doc.tsx
+++ src/documents/aio-doc/doc-aio-doc.tsx
@@ -11,81 +11,288 @@ export default function DOC_AIOForm(props:any) {
             goToHome={props.goToHome}
             nav={{
                 items: () => [
-                    { text: 'basic', id: 'basic', render: () => <Basic /> },
+                    { text: 'Ul', id: 'ul', render: () => <Ul /> },
+                    { text: 'h1 h2 h3 h4 h5 h6', id: 'h', render: () => <H /> },
+                    { text: 'String Tags', id: 'string tags', render: () => <StringTags /> },
+                    { text: 'Table', id: 'table', render: () => <Table /> },
+                    { text: 'Line', id: 'line', render: () => <Line /> },
+                    { text: 'Code', id: 'code', render: () => <Code /> },
+                    { text: 'List', id: 'list', render: () => <List /> },
                     
                 ]
             }}
         />
     )
 }
-function Basic() {
-    let [popup] = useState<AIOPopup>(new AIOPopup())
+function Ul() {
     let [doc] = useState<AIODoc>(new AIODoc())
-    function showCode() {
-        popup.addModal({
-            id: 'code',
-            header: { title: 'code' },
-            body: () => {
-                return new AIODoc().Code(
-                    `import AIOHighlighter from 'aio-highlighter';
-                    function Basic() {
-                        let [show, setShow] = useState<boolean>(true)
-                        let [ah] = useState<AIOHighlighter>(
-                            new AIOHighlighter({
-                                padding:12,
-                                items:(index)=>{
-                                    if(index === 0){
-                                        return {
-                                            dom:$('.rsa-navigation-item').eq(0),
-                                            html:'this tab show basic usage of aio-highlighter component'
-                                        }
-                                    }
-                                    if(index === 1){
-                                        return {
-                                            dom:$('.rsa-navigation-item').eq(1),
-                                            html:'this tab show usage of aio-highlighter mouseAccess Props'
-                                        }
-                                    }
-                                    if(index === 2){
-                                        return {
-                                            dom:$('.rsa-header-title'),
-                                            html:'this is title of page'
-                                        }
-                                    }
-                                    if(index === 3){
-                                        return {
-                                            dom:$('#go-to-home'),
-                                            html:'this button is for exit to home page'
-                                        }
-                                    }
-                                    setShow(false)
-                                }
-                            })
-                        )
-                        return (
-                            <div className='example'>
-                                {show && ah.render()}
-                            </div>
-                        )
-                    }`
-                        )
-                
-            }
-        })
-    }
     return (
-        <div className='example'>
-            {doc.H('My Title',3)}
+        <div className='example aio-doc-container'>
             {
                 doc.Ul([
-                    ['lorem','inpsum'],
-                    ['lorem','inpsum'],
-                    ['lorem','inpsum'],
-                    ['lorem','inpsum']
+                    {html:'lorem'},
+                    {
+                        html:'lorem',
+                        childs:[
+                            {html:'lorem'},
+                            {html:'lorem'},
+                            {html:'lorem'},
+                            {html:'lorem'}
+                        ]
+                    },
+                    {html:'lorem'},
+                    {html:'lorem'}
                 ])
             }
-            {doc.Mark('lorem',0)}
-            {popup.render()}
+            {doc.Code(
+`doc.Ul([
+    {html:'lorem'},
+    {
+        html:'lorem',
+        childs:[
+            {html:'lorem'},
+            {html:'lorem'},
+            {html:'lorem'},
+            {html:'lorem'}
+        ]
+    },
+    {html:'lorem'},
+    {html:'lorem'}
+])`
+            )}
+        </div>
+    )
+}
+
+
+function H() {
+    let [doc] = useState<AIODoc>(new AIODoc())
+    return (
+        <div className='example aio-doc-container'>
+            {doc.Tag('h6','My Title')}
+            {doc.Tag('h5','My Title')}
+            {doc.Tag('h4','My Title')}
+            {doc.Tag('h3','My Title')}
+            {doc.Tag('h2','My Title')}
+            {doc.Tag('h1','My Title')}
+            {doc.Code(
+`doc.Tag('h3','My Title')`
+)}
+            
+        </div>
+    )
+}
+
+function StringTags() {
+    let [doc] = useState<AIODoc>(new AIODoc())
+    let tbody = ['mark','sup','sub','em','b','strong','i','small','del'].map((o:any)=>{
+        return [o,doc.Tag(o,'lorem'),doc.Code(`doc.Tag('${o}','lorem')`)]
+    })
+    return (
+        <div className='example aio-doc-container'>
+            {
+                doc.Tag(
+                    'p',
+                    <>
+                        Lorem ipsum, dolor sit amet {doc.Tag('em','lorem')} adipisicing elit. Quos a.<br/>
+                        Adipisci beatae, accusamus iure {doc.Tag('mark','lorem')}, autem, doloribus nesciunt {doc.Tag('ins','lorem')} aperiam architecto laborum eaque velit vero corrupti.
+                        Nulla {doc.Tag('b','lorem')} quis ab, {doc.Tag('small','lorem')} ipsa autem.
+                        Placeat reiciendis, necessitatibus quos animi veritatis ad laboriosam sapiente odit aperiam repellat,
+                        Tempora nemo {doc.Tag('strong','lorem')}, sapiente {doc.Tag('del','lorem')} dolor {doc.Tag('sub','lorem')} quam quos quis {doc.Tag('sup','lorem')} quae! Mollitia dolore temporibus, sunt, corrupti cupiditate at numquam quo facere dignissimos, sequi aliquam maiores et. Libero, mollitia earum.
+                    </>
+                )
+            }
+            {
+                doc.Code(
+`doc.Tag(
+    'p',
+    <>
+        Lorem ipsum, dolor sit amet {doc.Tag('em','lorem')} adipisicing elit. Quos a.<br/>
+        Adipisci beatae, accusamus iure {doc.Tag('mark','lorem')}, autem, doloribus nesciunt {doc.Tag('ins','lorem')} aperiam architecto laborum eaque velit vero corrupti.
+        Nulla {doc.Tag('b','lorem')} quis ab, {doc.Tag('small','lorem')} ipsa autem.
+        Placeat reiciendis, necessitatibus quos animi veritatis ad laboriosam sapiente odit aperiam repellat,
+        Tempora nemo {doc.Tag('strong','lorem')}, sapiente {doc.Tag('del','lorem')} dolor {doc.Tag('sub','lorem')} quam quos quis {doc.Tag('sup','lorem')} quae! Mollitia dolore temporibus, sunt, corrupti cupiditate at numquam quo facere dignissimos, sequi aliquam maiores et. Libero, mollitia earum.
+    </>
+)`
+                )
+            }
+            {doc.Table(['tag','Preview','Code'],tbody)}
+        </div>
+    )
+}
+
+function Table() {
+    let [doc] = useState<AIODoc>(new AIODoc())
+    return (
+        <div className='example aio-doc-container'>
+            {
+                doc.Table(
+                    ['Props','Type','Default','Description'],
+                    [
+                        ['text','string','undefined','Set text of element'],
+                        ['text','string','undefined',<>accusamus iure {doc.Tag('mark','lorem')}, autem, doloribus nesciunt nulla</>],
+                        ['text','string','undefined','Set text of element'],
+                        ['text','string','undefined','Set text of element'],
+                        ['text','string','undefined','Set text of element']
+                    ]
+                )
+            }
+            {
+                doc.Code(
+`doc.Table({
+    thead:['Props','Type','Default','Description'],
+    tbody:[
+        ['text','string','undefined','Set text of element'],
+        ['text','string','undefined',<>accusamus iure {doc.Tag('mark','lorem')}, autem, doloribus nesciunt nulla</>],
+        ['text','string','undefined','Set text of element'],
+        ['text','string','undefined','Set text of element'],
+        ['text','string','undefined','Set text of element']
+    ]
+})`
+                )
+            }
+        </div>
+    )
+}
+
+function Line() {
+    let [doc] = useState<AIODoc>(new AIODoc())
+    return (
+        <div className='example aio-doc-container'>
+            {doc.Line('','1px dashed')}
+            {
+                doc.Code(
+`doc.Line('','1px dashed')`
+                )
+            }
+            {doc.Line('','2px dashed')}
+            {
+                doc.Code(
+`doc.Line('','2px dashed')`
+                )
+            }
+            {doc.Line('','1px dotted')}
+            {
+                doc.Code(
+`doc.Line('','1px dotted')`
+                )
+            }
+            {doc.Line('','2px dotted')}
+            {
+                doc.Code(
+`doc.Line('','2px dotted')`
+                )
+            }
+            {doc.Line('','1px solid')}
+            {
+                doc.Code(
+`doc.Line('','1px solid')`
+                )
+            }
+            {doc.Line('','2px solid')}
+            {
+                doc.Code(
+`doc.Line('','2px solid')`
+                )
+            }
+            {doc.Line('','1px solid','red')}
+            {
+                doc.Code(
+`doc.Line('','1px solid','red')`
+                )
+            }
+            {doc.Line('','3px solid','dodgerblue')}
+            {
+                doc.Code(
+`doc.Line('','3px solid','dodgerblue')`
+                )
+            }
+            {doc.Line('My Text')}
+            {
+                doc.Code(
+`doc.Line('My Text')`
+                )
+            }
+            {doc.Line('My Text','2px solid','dodgerblue')}
+            {
+                doc.Code(
+`doc.Line('My Text','2px solid','dodgerblue')`
+                )
+            }
+        </div>
+    )
+}
+
+function Code() {
+    let [doc] = useState<AIODoc>(new AIODoc())
+    return (
+        <div className='example aio-doc-container'>
+            {
+                doc.Code(
+`function Example(){
+    return (
+        <div>
+            some text
+        </div>
+    )
+}`
+                )
+            }
+            {
+                doc.Code(
+`doc.Code(
+    function Example(){
+        return (
+            <div>
+                some text
+            </div>
+        )
+    }
+)`
+                )
+            }
+        </div>
+    )
+}
+
+
+function List() {
+    let [doc] = useState<AIODoc>(new AIODoc())
+    return (
+        <div className='example aio-doc-container'>
+            {doc.List([
+                ['Tag','h3','My Title'],
+                [
+                    'Ul',
+                    `[
+                        {"html":"lorem"},
+                        {
+                            "html":"lorem",
+                            "childs":[
+                                {"html":"lorem"},
+                                {"html":"lorem"},
+                                {"html":"lorem"},
+                                {"html":"lorem"}
+                            ]
+                        },
+                        {"html":"lorem"},
+                        {"html":"lorem"}
+                    ]`
+                ],
+                [
+                    'Tag',
+                    'p',
+                    `<>
+                        Lorem ipsum, dolor sit amet {doc.Tag('em','lorem')} adipisicing elit. Quos a.<br/>
+                        Adipisci beatae, accusamus iure {doc.Tag('mark','lorem')}, autem, doloribus nesciunt {doc.Tag('ins','lorem')} aperiam architecto laborum eaque velit vero corrupti.
+                        Nulla {doc.Tag('b','lorem')} quis ab, {doc.Tag('small','lorem')} ipsa autem.
+                        Placeat reiciendis, necessitatibus quos animi veritatis ad laboriosam sapiente odit aperiam repellat,
+                        Tempora nemo {doc.Tag('strong','lorem')}, sapiente {doc.Tag('del','lorem')} dolor {doc.Tag('sub','lorem')} quam quos quis {doc.Tag('sup','lorem')} quae! Mollitia dolore temporibus, sunt, corrupti cupiditate at numquam quo facere dignissimos, sequi aliquam maiores et. Libero, mollitia earum.
+                    </>`
+                ]
+            ])}
+            
+            
         </div>
     )
 }
--- src/npm/aio-doc/aio-doc.tsx
+++ src/npm/aio-doc/aio-doc.tsx
@@ -1,45 +1,80 @@
 import React, { Component, FC, useEffect } from "react";
 import Prism from 'prismjs';
 import './index.css';
+import AIOInput from "../aio-input";
+type I_li = {html:React.ReactNode,childs?:I_li[]}
+type rn = React.ReactNode
+type I_tag = 'b'|'strong'|'i'|'em'|'mark'|'small'|'del'|'ins'|'sub'|'sup'|'h1'|'h2'|'h3'|'h4'|'h5'|'h6'|'p'
 export default class AIODoc {
-    Code:(code:string, language?:'js' | 'css', style?:any)=>React.ReactNode;
-    Ul:(list:[string,string][])=>React.ReactNode;
-    H:(text:string,num:number)=>React.ReactNode;
-    Line:(text?:string)=>React.ReactNode;
-    Mark:(text:string,color?:number)=>React.ReactNode;
+    Code:(code:string, language?:'js' | 'css', style?:any)=>rn;
+    Ul:(list:I_li[])=>rn;
+    Line:(text:rn,style?:string,color?:string)=>rn;
+    Table:(thead:string[],tbody:rn[][],attrs?:any)=>rn;
+    Tag:(tag:I_tag,text:rn,attrs?:{[key:string]:any})=>rn;
+    // Acardion:(title:string,html:rn)=>rn;
+    List:(p:['Code'|'Ul'|'Table'|'Tag'|'Line',any?,any?,any?][])=>rn
     constructor(){
         this.Code = (code, language, style)=>{
             return <PrismCode code={code} language={language} style={style} />
         }
-        this.Ul = (list) => {
+        this.Ul = (childs) => {
             return (
-                <ul className='aio-doc-list'>
+                <ul className='aio-doc-element aio-doc-ul'>
                     {
-                        list.map(([desc,title]) => {
-                            return (
-                                <li>
-                                    {!!title && this.Mark(title)} {desc}
-                                </li>
-                            )
+                        childs.map((o) => {
+                            let {childs = []} = o;
+                            let Childs:any[] = Array.isArray(childs)?childs:[childs]
+                            return (<li>{o.html}{!!(childs as any[]).length && this.Ul(Childs)}</li>)
                         })
                     }
                 </ul>
             )
         }
-        this.Mark = (text,color = 0)=>{
-            return <mark className={`aio-doc-mark aio-doc-mark${color}`}>{text}</mark>
-        }
-        this.H = (text,num = 3) => {
-            const Element = `h${num}` as keyof JSX.IntrinsicElements; 
-            return <Element className='aio-doc-titr'>{text}</Element>
-        }
-        this.Line = (text)=>{
+        this.List = (list)=>{
             return (
-                <div className='aio-doc-splitter'>
+                <>
+                    {list.map(([fn,p1,p2,p3])=>{
+                        let P1,P2,P3;
+                        try{P1 = JSON.parse(p1)}catch{P1 = p1}
+                        try{P2 = JSON.parse(p2)}catch{P2 = p2}
+                        try{P3 = JSON.parse(p3)}catch{P2 = p2}
+                        return this[fn](P1,P2,P3)
+                    })}
+                </>
+            )
+        }
+        this.Tag = (tag,html,attrs = {})=>{
+            const Tag = tag
+            return <Tag {...attrs}>{html}</Tag>
+        }
+        this.Line = (text = '',style = '1px solid',color = '#ddd')=>{
+            return (
+                <div className={`aio-doc-splitter`} style={{borderBottom:style,color}}>
                     {!!text && <div className='aio-doc-splitter-text'>{text}</div>}
                 </div>
             )
         }
+        this.Table = (thead,tbody,attrs)=>{
+            return (
+                <table border={1} {...attrs}>
+                    <thead>{thead.map((th)=><th>{th}</th>)}</thead>
+                    <tbody>{tbody.map((tr,i)=><tr key={i}>{tr.map((td,i)=><td key={i}>{td}</td>)}</tr>)}</tbody>
+                </table>
+            )
+        }
+        // this.Acardion = (title,html = <div></div>)=>{
+        //     return (
+        //         <AIOInput
+        //             type='acardion'
+        //             options={[{title}]}
+        //             option={{
+        //                 text:'option.title',
+        //                 value:'option.title'
+        //             }}
+        //             body={()=>({html})}
+        //         />
+        //     )
+        // }
     }
 }
 const PrismCode:FC<{code:string, language?:'js' | 'css', style?:any}> = ({code,language = 'javascript',style = {}}) => {
